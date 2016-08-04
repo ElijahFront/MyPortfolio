144cc81d9b415277164c7e8a4aac66562110b752
Ilya <cuprum19930@gmail.com>
2016-08-05 00:36:30 +0500

    баг в меню блога на планшете; обновление скилов

--- routes/skills.js
+++ routes/skills.js
@@ -1,11 +1,38 @@
 var SkillsData = require('../models/skills').Skills;
 
-exports.post = function (req, res) {
+exports.post = function (req, res, next) {
+
     console.log('Got POST req on', req.url);
+    console.log(req.body);
 
-    var skills = new SkillsData(req.body);
+    SkillsData.findOne({}, function (err, skill) {
+        if (err) return next(err);
 
-    skills.save();
+        if (skill){
+            var id = skill._id;
+            SkillsData.findByIdAndUpdate(id, {
+                html: req.body.html,
+                css: req.body.css,
+                js: req.body.js,
+                php: req.body.php,
+                node: req.body.node,
+                mongo: req.body.mongo,
+                git: req.body.git,
+                gulp: req.body.gulp,
+                bower: req.body.bower
+            }, function (er) {
+                if (er) return next(er);
+                res.sendStatus(200)
+            })
+        } else {
+            var skills = new SkillsData(req.body);
 
-    res.end()
+            skills.save(function (e) {
+                if (e) return next(e);
+                res.sendStatus(200)
+            });
+        }
+    });
+
+
 };
--- source/js/app.js
+++ source/js/app.js
@@ -472,21 +472,44 @@ $(window).on('mousemove', function(e){
             git = $('#admin__git').val(),
             gulp = $('#admin__gulp').val(),
             bower = $('#admin__bower').val();
+        if (html == ''||css == ''||php == ''||node == ''||mongo == ''||git == ''||gulp == ''||gulp == ''||bower == ''){
+            alert('Пожалуйста, не оставляйте пустые поля')
+        } else {
 
-        var SkillsData = {
-            html: html,
-            css: css,
-            js: js,
-            php: php,
-            node: node,
-            mongo: mongo,
-            git: git,
-            gulp: gulp,
-            bower: bower
-        };
+            var SkillsData = {
+                html: html,
+                css: css,
+                js: js,
+                php: php,
+                node: node,
+                mongo: mongo,
+                git: git,
+                gulp: gulp,
+                bower: bower
+            };
 
 
-        sendXHR('/skillsPost', SkillsData);
+            //sendXHR('/skillsPost', SkillsData);
+            $.ajax({
+                type: 'POST',
+                url: '/skillsPost',
+                data: SkillsData,
+                statusCode:{
+                    200 : function () {
+                        alert('Скилы сохранены!');
+                        $('#admin__html').val('');
+                        $('#admin__css').val('');
+                        $('#admin__js').val('');
+                        $('#admin__php').val('');
+                        $('#admin__node').val('');
+                        $('#admin__mongo').val('');
+                        $('#admin__git').val('');
+                        $('#admin__gulp').val('');
+                        $('#admin__bower').val('');
+                    }
+                }
+            })
+        }
 
     });
     
@@ -504,8 +527,28 @@ $(window).on('mousemove', function(e){
             text: text
         };
         
-        sendXHR('/saveBlogArticles', blogData)
+        //sendXHR('/saveBlogArticles', blogData)
+        $.ajax({
+            type: 'POST',
+            url: '/saveBlogArticles',
+            data: blogData,
+            statusCode:{
+                200 : function () {
+                    alert('Статья сохранена!');
+                    $('.admin__blog_form-title').val('');
+                    $('.admin__blog_form-date').val('');
+                    $('.admin__blog_form-text').val('');
+                }
+            }
+        })
         
+    });
+
+    $('#save_work').on('click', function (e) {
+
+        e.preventDefault();
+
+        alert('Извините, данный функционал пока в разработке. Попробуйте написать статью в блог')
     })
 
 }());
